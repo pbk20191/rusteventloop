4ba427a9cf3dc522b2a917a81bf7b592250bf99a
park-byeong-gwan <impbk2002@gmail.com>
2025-02-02 13:13:31 +0900

    refactor cfrunloop

--- src/osxloop.rs
+++ src/osxloop.rs
@@ -8,44 +8,41 @@ use std::future::Future;
 )]
 pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
     use std::{
+        cell::OnceCell,
         future::Future,
         os::raw::c_void,
-        sync::{Arc, Mutex, Weak},
-        time::Duration,
-        cell::{ Cell, UnsafeCell, OnceCell}
+        // sync::{Arc, Weak},
+        rc::{Weak, Rc},
+        time::Duration
     };
-    use std::ptr::{ null_mut, null };
-    use block2::{Block, StackBlock};
+    use std::ptr::{null, null_mut};
     use compio::driver::AsRawFd;
-    use compio::runtime::{Runtime, event::Event};
+    use compio::runtime::Runtime;
     use core_foundation::{
-        base::TCFType,
-        base::{ CFAllocatorRef, CFIndex, CFOptionFlags, Boolean, CFType, FromVoid,
-                CFRetain, CFRelease, ToVoid
-        },
-        date::{CFAbsoluteTime, CFTimeInterval, CFAbsoluteTimeGetCurrent},
-        filedescriptor::{CFFileDescriptor, CFFileDescriptorRef, kCFFileDescriptorReadCallBack, CFFileDescriptorContext, CFFileDescriptorEnableCallBacks},
-        runloop::{CFRunLoop, CFRunLoopRef, CFRunLoopStop, kCFRunLoopDefaultMode,CFRunLoopSourceContext,
-                  CFRunLoopObserver, CFRunLoopTimerContext, CFRunLoopObserverContext, CFRunLoopTimerInvalidate, CFRunLoopWakeUp,
-                  CFRunLoopObserverInvalidate, kCFRunLoopBeforeWaiting, kCFRunLoopAfterWaiting, kCFRunLoopBeforeTimers, kCFRunLoopEntry,
-                  CFRunLoopTimerGetContext, CFRunLoopTimerSetNextFireDate,CFRunLoopSource,CFRunLoopSourceSignal, CFRunLoopSourceCreate, CFRunLoopSourceInvalidate,
-                  CFRunLoopObserverRef, CFRunLoopTimer, CFRunLoopTimerRef, CFRunLoopActivity,kCFRunLoopAllActivities, CFRunLoopObserverCreate, CFRunLoopGetCurrent
-        },
-        string::CFStringRef,
+        base::TCFType
+
+        ,
+        filedescriptor::{kCFFileDescriptorReadCallBack, CFFileDescriptor, CFFileDescriptorContext, CFFileDescriptorRef},
+        runloop::{kCFRunLoopAllActivities, kCFRunLoopBeforeWaiting, kCFRunLoopDefaultMode, CFRunLoop,
+                  CFRunLoopActivity, CFRunLoopGetCurrent, CFRunLoopObserver, CFRunLoopObserverContext, CFRunLoopObserverCreate,
+                  CFRunLoopObserverInvalidate, CFRunLoopObserverRef
+                  , CFRunLoopSource, CFRunLoopSourceContext, CFRunLoopSourceCreate, CFRunLoopSourceInvalidate, CFRunLoopSourceSignal,
+                  CFRunLoopStop, CFRunLoopTimer, CFRunLoopTimerContext, CFRunLoopTimerInvalidate, CFRunLoopTimerRef, CFRunLoopTimerSetNextFireDate, CFRunLoopWakeUp
+        }
+        ,
     };
 
     struct CFContextInfo {
         source: OnceCell<CFRunLoopSource>,
         timer: OnceCell<CFRunLoopTimer>,
         observer: OnceCell<CFRunLoopObserver>,
-        runtime: Weak<Runtime>,
-        dummy_source: CFRunLoopSource,
+        runtime: Weak<Runtime>
     }
     
     struct CFRunLoopRuntime {
-        runtime: Arc<Runtime>,
+        runtime: Rc<Runtime>,
         fd_source: CFFileDescriptor,
-        context: Arc<CFContextInfo>
+        context: Rc<CFContextInfo>
     }
 
     extern "C" fn callback(
@@ -55,8 +52,7 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
     ) {
         let context = unsafe {
             let ptr = info as *const CFContextInfo;
-            Arc::increment_strong_count(ptr);
-            Arc::from_raw(ptr)
+            &*ptr
         };
         // let _ = context_ref.clone().into_raw();
         // let Some(context) = context_ref.upgrade() else { return;};
@@ -74,20 +70,14 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
     extern "C" fn timer(timer: CFRunLoopTimerRef, info: *mut c_void) {
         let context = unsafe {
             let ptr = info as *const CFContextInfo;
-            Arc::increment_strong_count(ptr);
-            Arc::from_raw(ptr)
+            &*ptr
         };
         // print!("{}", context_ref.weak_count());
         // let Some(context) = context_ref.upgrade() else { return;};
         let Some(runtime) = context.runtime.upgrade() else { return;};
         runtime.poll_with(Some(Duration::ZERO));
         let remaining_tasks = runtime.run();
-        let dummy = context.dummy_source.as_concrete_TypeRef();
-        unsafe {
-            CFRunLoopSourceSignal(dummy);
-        }
         let Some(timeout) = runtime.current_timeout() else { return; };
-
         unsafe {
             CFRunLoopTimerSetNextFireDate(timer, timeout.as_secs_f64())
         }
@@ -96,19 +86,12 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
     extern "C" fn observe(observer: CFRunLoopObserverRef, activity: CFRunLoopActivity, info: *mut c_void) {
         let context = unsafe {
             let ptr = info as *const CFContextInfo;
-            Arc::increment_strong_count(ptr);
-            Arc::from_raw(ptr)
+            &*ptr
         };
         if activity == kCFRunLoopBeforeWaiting {
             let Some(runtime) = context.runtime.upgrade() else { return;};
-            runtime.poll_with(Some(Duration::ZERO));
             let remaining_tasks = runtime.run();
-            let dummy = context.dummy_source.as_concrete_TypeRef();
-            unsafe {
-                CFRunLoopSourceSignal(dummy);
-            }
             let Some(timeout) = runtime.current_timeout() else { return; };
-
             unsafe {
                 CFRunLoopTimerSetNextFireDate(context.timer.get().unwrap().as_concrete_TypeRef(), timeout.as_secs_f64())
             }
@@ -119,7 +102,7 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
 
         let ptr = info as * const CFContextInfo;
         unsafe {
-            Arc::increment_strong_count(ptr);
+            Rc::increment_strong_count(ptr);
         }
         return ptr as *const c_void
     }
@@ -128,7 +111,7 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
 
         let ptr = info as * const CFContextInfo;
         unsafe {
-            Arc::decrement_strong_count(ptr);
+            Rc::decrement_strong_count(ptr);
         }
         
     }
@@ -144,36 +127,18 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
 
         pub fn new() -> Self {
             let runtime = Runtime::new().unwrap();
-            let wrapper = Arc::new(runtime);
+            let wrapper = Rc::new(runtime);
             
            
-            let arc_context = Arc::new(CFContextInfo{
+            let arc_context = Rc::new(CFContextInfo{
                 source: OnceCell::new(),
                 timer: OnceCell::new(),
                 observer: OnceCell::new(),
-                runtime: Arc::downgrade(&wrapper),
-                dummy_source: unsafe {
-                    let mut context = CFRunLoopSourceContext{
-                        version: 0,
-                        info: null_mut(),
-                        retain: None,
-                        release: None,
-                        copyDescription: None,
-                        equal: None,
-                        hash: None,
-                        schedule: None,
-                        cancel: None,
-                        perform: dummy_perform,
-                    };
-                    CFRunLoopSource::wrap_under_create_rule(
-                        CFRunLoopSourceCreate(null(), 0, &mut context)
-                    )
-                }
+                runtime: Rc::downgrade(&wrapper),
             });
 
 
-
-            let raw_ptr = Arc::into_raw(arc_context);
+            let raw_ptr = Rc::into_raw(arc_context);
             let a = CFFileDescriptorContext {
                 version: 0,
                 info: raw_ptr as * mut c_void,
@@ -220,8 +185,7 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
             run_loop.add_source(&source, unsafe { kCFRunLoopDefaultMode });
             run_loop.add_timer(&run_loop_timer, unsafe { kCFRunLoopDefaultMode });
             run_loop.add_observer(&observer, unsafe { kCFRunLoopDefaultMode});
-            let reconstructed_context = unsafe { Arc::from_raw(raw_ptr) };
-            run_loop.add_source(&reconstructed_context.dummy_source, unsafe { kCFRunLoopDefaultMode });
+            let reconstructed_context = unsafe { Rc::from_raw(raw_ptr) };
             let _ = reconstructed_context.timer.set(run_loop_timer);
             let _ = reconstructed_context.source.set(source);
             let _ = reconstructed_context.observer.set(observer);
@@ -229,25 +193,45 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
         }
 
         pub fn block_on<F: Future>(&self, future: F) -> F::Output {
+
+            let signal =  unsafe {
+                let mut context = CFRunLoopSourceContext{
+                    version: 0,
+                    info: null_mut(),
+                    retain: None,
+                    release: None,
+                    copyDescription: None,
+                    equal: None,
+                    hash: None,
+                    schedule: None,
+                    cancel: None,
+                    perform: dummy_perform,
+                };
+                CFRunLoopSource::wrap_under_create_rule(
+                    CFRunLoopSourceCreate(null(), 0, &mut context)
+                )
+            };
             self.runtime.enter(|| {
                 let mut result = None;
                 let run_loop = CFRunLoop::get_current();
-                let signal = self.context.dummy_source.as_concrete_TypeRef();
                 unsafe {
                     self.runtime
                         .spawn_unchecked(async {
                             result = Some(future.await);
-                            CFRunLoopSourceSignal(signal);
+                            CFRunLoopSourceSignal(signal.as_concrete_TypeRef());
                             CFRunLoopWakeUp(run_loop.as_concrete_TypeRef());
-                            // CFRunLoopStop(run_loop.as_concrete_TypeRef());
                         })
                 }
                     .detach();
+                CFRunLoop::get_current().add_source(&signal, unsafe { kCFRunLoopDefaultMode});
                 self.fd_source
                     .enable_callbacks(kCFFileDescriptorReadCallBack);    
 
                 loop {
                     if let Some(result) = result.take() {
+                        unsafe {
+                            CFRunLoopSourceInvalidate(signal.as_concrete_TypeRef());
+                        }
                         break result;
                     }
                     let run_result = CFRunLoop::run_in_mode(
@@ -274,16 +258,10 @@ pub(crate) fn apple_run_loop<F: Future>(future: F) -> F::Output {
         fn drop(&mut self) {
             // break reference cycle!
             self.fd_source.invalidate();
-            let source = self.context.dummy_source.as_concrete_TypeRef();
             let observer = self.context.observer.get().unwrap().as_concrete_TypeRef();
             unsafe {
-                if (source != null_mut()) {
-                    CFRunLoopSourceInvalidate(source);
-
-                }
                 CFRunLoopObserverInvalidate(observer);
                 CFRunLoopTimerInvalidate(self.context.timer.get().unwrap().as_concrete_TypeRef());
-                // CFRunLoopObserverInvalidate(self.context.source.take().unwrap().as_concrete_TypeRef());
             }
            
         }
@@ -307,6 +285,7 @@ fn test_loop() {
     use compio::runtime::event::Event;
     use core_foundation::runloop::{kCFRunLoopDefaultMode, CFRunLoop, CFRunLoopRef};
     use core_foundation::string::CFStringRef;
+    use core_foundation::base::TCFType;
     
     apple_run_loop(async {
         compio::runtime::time::sleep(Duration::from_secs(1)).await;
