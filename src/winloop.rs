5f269ae600c0b3e40cbc7beac5c72afc84c486f6
Byeong Gwan <impbk2002@gmail.com>
2025-02-06 21:49:21 +0900

    fix window message loop using libffi

--- src/winloop.rs
+++ src/winloop.rs
@@ -21,7 +21,10 @@ pub(self) fn test_window() {
     use std::time::Duration;
     use compio::runtime::event::{Event, EventHandle};
     use windows_sys::Win32::Foundation::HWND;
-    use windows_sys::Win32::UI::WindowsAndMessaging::{KillTimer, MessageBoxW, SetTimer, MB_OK};
+    use windows_sys::Win32::UI::WindowsAndMessaging::{
+        KillTimer, MessageBoxW, SetTimer, MB_OK
+    };
+    
     message_queue(async{
         compio::runtime::time::sleep(Duration::from_secs(1)).await;
 
@@ -59,6 +62,10 @@ fn message_queue_impl<J: job_specialization::JobTrait>(job: J, input: J::Input) 
     use std::{mem::MaybeUninit, time::Duration};
     use compio::driver::AsRawFd;
     use compio::runtime::Runtime;
+    use libffi::high::{
+        Closure3, Closure4
+    };
+
     use windows_sys::Win32::{
         Foundation::{HANDLE, HWND, LPARAM, LRESULT, WAIT_FAILED, WPARAM},
         System::Threading::{
@@ -76,56 +83,6 @@ fn message_queue_impl<J: job_specialization::JobTrait>(job: J, input: J::Input) 
     struct OwnedTimer {
         native: usize
     }
-    scoped_tls::scoped_thread_local!(static CURRENT_TIMER: OwnedTimer);
-
-    extern "system" fn message_proc(code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-
-        match code as u32 {
-            MSGF_DIALOGBOX | MSGF_MESSAGEBOX => {
-                Runtime::with_current(|runtime| {
-                    
-                    runtime.poll_with(Some(Duration::ZERO));
-                    runtime.run();
-                    CURRENT_TIMER.with(|timer| {
-                        match runtime.current_timeout() {
-                            None =>  {
-                                // this procedure is called rapidly
-                                // so it would be better to do nothing, rather than updating timer to distant future
-                            }
-                            Some(timeout) => unsafe {
-                                SetTimer(0, timer.native, timeout.as_millis() as u32, Some(timer_proc));
-                            }
-                        }
-                    });
-
-                });
-            }
-            MSGF_MENU => {}
-            MSGF_SCROLLBAR => {}
-            _ => {}
-        }
-
-        unsafe {
-            CallNextHookEx(0, code, w_param, l_param)
-        }
-    }
-
-    unsafe extern "system" fn timer_proc(hwnd: HWND, _msg: u32, id: usize, _dwtime: u32) {
-
-        Runtime::with_current(|runtime| {
-            runtime.poll_with(Some(Duration::ZERO));
-            runtime.run();
-            match runtime.current_timeout() {
-                None => {
-                    SetTimer(hwnd, id, USER_TIMER_MAXIMUM, Some(timer_proc));
-                }
-                Some(timeout) => {
-                    SetTimer(hwnd, id, timeout.as_millis() as u32, Some(timer_proc));
-                }
-            }
-        });
-    }
-
 
     impl Drop for OwnedTimer {
         
@@ -148,92 +105,151 @@ fn message_queue_impl<J: job_specialization::JobTrait>(job: J, input: J::Input) 
             }
         }
     }
-    let _hook =  {
-        let value = unsafe {
-            SetWindowsHookExW(WH_MSGFILTER, Some(message_proc), 0, GetCurrentThreadId())
-        };
-        if value == 0 {
-            panic!("{:?}", std::io::Error::last_os_error());
-        }
-        OwnedWindowHook{ native: value }
-    };
-    let timer =  {
-        let handle = unsafe{
-            SetTimer(0, 0, 1000, Some(timer_proc))
-        };
-        if handle == 0 {
-            panic!("{:?}", std::io::Error::last_os_error());
-        }
-        
-        OwnedTimer{ native:handle }
-    };
     let runtime = Runtime::new().unwrap();
     let runtime_ref = &runtime;
-    runtime.enter(move || {
-        CURRENT_TIMER.set(&timer,  move || {
-            let mut my_job = job;
-            my_job.spawn(input, runtime_ref);
-            use std::ptr::null;
-            'outer: loop {
-                runtime_ref.poll_with(Some(Duration::ZERO));
-                let remaining_tasks = runtime_ref.run();
-                if let Some(result) = my_job.check(null()) {
-                    break result;
+
+
+    runtime_ref.enter( move || {
+        use std::cell::RefCell;
+        use std::mem::transmute;
+        use windows_sys::Win32::UI::WindowsAndMessaging::{MSGF_DIALOGBOX, MSGF_MENU, MSGF_MESSAGEBOX, MSGF_SCROLLBAR, TIMERPROC};
+
+        let timer =  {
+            let handle = unsafe{
+                SetTimer(0, 0, 1000, None)
+            };
+            if handle == 0 {
+                panic!("{:?}", std::io::Error::last_os_error());
+            }
+
+            OwnedTimer{ native:handle }
+        };
+        let timer_ref = &timer;
+        let timer_proc :RefCell<TIMERPROC> = RefCell::new(None);
+        let timer_proc_ref = &timer_proc;
+        let timer_cb = move |hwnd: HWND, _msg: u32, id: usize, _dwtime: u32| {
+
+            runtime_ref.poll_with(Some(Duration::ZERO));
+            runtime_ref.run();
+
+            match runtime_ref.current_timeout() {
+                None => unsafe {
+                    SetTimer(0, timer_ref.native, USER_TIMER_MAXIMUM, *timer_proc_ref.borrow());
                 }
-                let timeout = if remaining_tasks {
-                    Some(Duration::ZERO)
-                } else {
-                    runtime_ref.current_timeout()
-                };
-                match timeout {
-                    Some(timeout) => {
-                        unsafe {
-                            SetTimer(
-                                0, timer.native, timeout.as_millis() as u32, Some(timer_proc)
-                            )
-                        };
-                    }
-                    None => {
-                        unsafe {
-                            SetTimer(0, timer.native, USER_TIMER_MAXIMUM, Some(timer_proc));
+                Some(timeout) => unsafe {
+                    SetTimer(0, timer_ref.native, timeout.as_millis() as u32, *timer_proc_ref.borrow());
+
+                }
+            }
+        };
+        let timer_closure = Closure4::new(&timer_cb);
+
+        unsafe {
+
+            timer_proc.replace(Some(transmute(*timer_closure.code_ptr())));
+
+            SetTimer(0, timer.native, 1000, Some(transmute(*timer_closure.code_ptr())));
+        };
+
+        let message_proc = move |code: i32, w_param: WPARAM, l_param: LPARAM| -> LRESULT {
+
+            match code as u32 {
+                MSGF_DIALOGBOX | MSGF_MESSAGEBOX => {
+                    runtime_ref.poll_with(Some(Duration::ZERO));
+                    runtime_ref.run();
+                    match runtime_ref.current_timeout() {
+                        None =>  {
+                            // this procedure is called rapidly
+                            // so it would be better to do nothing, rather than updating timer to distant future
+                        }
+                        Some(timeout) => unsafe {
+                            SetTimer(0, timer_ref.native, timeout.as_millis() as u32, *timer_proc_ref.borrow());
                         }
                     }
-                }
 
-                let handle = runtime_ref.as_raw_fd() as HANDLE;
-                let res = unsafe {
-                    MsgWaitForMultipleObjectsEx(
-                        1,
-                        &handle,
-                        INFINITE,
-                        QS_ALLINPUT,
-                        MWMO_ALERTABLE | MWMO_INPUTAVAILABLE,
-                    )
-                };
-                if res == WAIT_FAILED {
-                    panic!("{:?}", std::io::Error::last_os_error());
                 }
-                let mut msg = MaybeUninit::uninit();
-                while unsafe{ PeekMessageW(msg.as_mut_ptr(), 0, 0, 0, PM_REMOVE)} != 0 {
-                    let msg = unsafe { msg.assume_init() };
-                    if msg.message == WM_QUIT {
-                        let k = &msg;
-                        use std::ffi::c_void;
-
-                        let k2 = (k as *const MSG) as *const c_void;
-                        if let Some(value) = my_job.check(k2) {
-                            break 'outer value;
-                        }
-                    }
+                MSGF_MENU => {}
+                MSGF_SCROLLBAR => {}
+                _ => {}
+            }
+            unsafe {
+                CallNextHookEx(0, code, w_param, l_param)
+            }
+        };
+        let _hook_store = Closure3::new(&message_proc);
+        let _hook =  {
+            let store = &_hook_store;
+            let value = unsafe {
+                SetWindowsHookExW(WH_MSGFILTER, Some(transmute(*store.code_ptr())), 0, GetCurrentThreadId())
+            };
+            if value == 0 {
+                panic!("{:?}", std::io::Error::last_os_error());
+            }
+            OwnedWindowHook{ native: value }
+        };
+        let mut my_job = job;
+        my_job.spawn(input, runtime_ref);
+        
+        use std::ptr::null;
+        'outer: loop {
+            runtime_ref.poll_with(Some(Duration::ZERO));
+            let remaining_tasks = runtime_ref.run();
+            if let Some(result) = my_job.check(null()) {
+                break result;
+            }
+            let timeout = if remaining_tasks {
+                Some(Duration::ZERO)
+            } else {
+                runtime_ref.current_timeout()
+            };
+            match timeout {
+                Some(timeout) => {
                     unsafe {
-                        if IsDialogMessageW(GetAncestor(msg.hwnd, GA_ROOT), &msg) == 0 {
-                            TranslateMessage(&msg);
-                            DispatchMessageW(&msg);
-                        }
+                        SetTimer(
+                            0, timer.native, timeout.as_millis() as u32, *timer_proc.borrow()
+                        )
+                    };
+                }
+                None => {
+                    unsafe {
+                        SetTimer(0, timer.native, USER_TIMER_MAXIMUM, *timer_proc.borrow());
                     }
                 }
             }
-        })
+
+            let handle = runtime_ref.as_raw_fd() as HANDLE;
+            let res = unsafe {
+                MsgWaitForMultipleObjectsEx(
+                    1,
+                    &handle,
+                    INFINITE,
+                    QS_ALLINPUT,
+                    MWMO_ALERTABLE | MWMO_INPUTAVAILABLE,
+                )
+            };
+            if res == WAIT_FAILED {
+                panic!("{:?}", std::io::Error::last_os_error());
+            }
+            let mut msg = MaybeUninit::uninit();
+            while unsafe{ PeekMessageW(msg.as_mut_ptr(), 0, 0, 0, PM_REMOVE)} != 0 {
+                let msg = unsafe { msg.assume_init() };
+                if msg.message == WM_QUIT {
+                    let k = &msg;
+                    use std::ffi::c_void;
+
+                    let k2 = (k as *const MSG) as *const c_void;
+                    if let Some(value) = my_job.check(k2) {
+                        break 'outer value;
+                    }
+                }
+                unsafe {
+                    if IsDialogMessageW(GetAncestor(msg.hwnd, GA_ROOT), &msg) == 0 {
+                        TranslateMessage(&msg);
+                        DispatchMessageW(&msg);
+                    }
+                }
+            }
+        }
     })
 }
 
